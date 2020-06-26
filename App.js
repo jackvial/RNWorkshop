418bcf3d9ce05ddc99df73bf1d5760af9aaceef6
Jack Vial <vialjack@gmail.com>
2020-06-25 20:35:48 -0400

    Install react navigation

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {
   SafeAreaView,
   StyleSheet,
